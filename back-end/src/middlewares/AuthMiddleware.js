7d471b7a06cf6f1d31fe10cc1174b7376cf242be
kiritokun0909 <hoduchoang0909@gmail.com>
2025-03-24 15:28:33 +0700

    - add api to get list liked/followed manga for user - add api to check user like/follow manga or not - add api for user to like/follow/unlike/unfollow manga - add HandleCode for const value used in filtering

--- back-end/src/middlewares/AuthMiddleware.js
+++ back-end/src/middlewares/AuthMiddleware.js
@@ -1,7 +1,11 @@
-const jwt = require("jsonwebtoken");
-const { StatusCodes } = require("http-status-codes");
-const { getHeaderToken, getUserRoleFromToken } = require("../utils/TokenUtil");
-const Messages = require("../utils/Messages");
+const jwt = require('jsonwebtoken');
+const { StatusCodes } = require('http-status-codes');
+const {
+  getHeaderToken,
+  getUserRoleFromToken,
+  decodeToken,
+} = require('../utils/TokenUtil');
+const Messages = require('../utils/Messages');
 
 function AuthMiddleware(allowedRoles = []) {
   return (req, res, next) => {
@@ -20,13 +24,16 @@ function AuthMiddleware(allowedRoles = []) {
           .send(Messages.ERROR.ACCESS_DENIED_NO_PERMISSION);
       }
 
+      const decoded = decodeToken(token);
+      req.user = decoded;
+
       next();
     } catch (error) {
-      if (error.name === "TokenExpiredError") {
+      if (error.name === 'TokenExpiredError') {
         return res
           .status(StatusCodes.UNAUTHORIZED)
           .send(Messages.ERROR.TOKEN_EXPIRED);
-      } else if (error.name === "JsonWebTokenError") {
+      } else if (error.name === 'JsonWebTokenError') {
         return res
           .status(StatusCodes.UNAUTHORIZED)
           .send(Messages.ERROR.TOKEN_INVALID);
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,23 +1,25 @@
-const siteRouter = require("./siteRoute.js");
+const siteRouter = require('./siteRoute.js');
 
-const testRouter = require("./TestRoute.js");
+const testRouter = require('./TestRoute.js');
 
-const authRouter = require("./AuthRoute.js");
+const authRouter = require('./AuthRoute.js');
+const userRouter = require('./UserRoute.js');
 
-const mangaRouter = require("./MangaRoute.js");
-const chapterRouter = require("./ChapterRoute.js");
-const genreRouter = require("./GenreRoute.js");
+const mangaRouter = require('./MangaRoute.js');
+const chapterRouter = require('./ChapterRoute.js');
+const genreRouter = require('./GenreRoute.js');
 
 function route(app) {
-  app.use("/", siteRouter);
+  app.use('/', siteRouter);
 
-  app.use("/test", testRouter);
+  app.use('/test', testRouter);
 
-  app.use("/auth", authRouter);
+  app.use('/auth', authRouter);
+  app.use('/user', userRouter);
 
-  app.use("/mangas", mangaRouter);
-  app.use("/chapters", chapterRouter);
-  app.use("/genres", genreRouter);
+  app.use('/mangas', mangaRouter);
+  app.use('/chapters', chapterRouter);
+  app.use('/genres', genreRouter);
 }
 
 module.exports = route;
